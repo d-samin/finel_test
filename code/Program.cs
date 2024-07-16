c1f53615089a60ebfb5ff7b8750cf0916bdb6764
Samina <dauylbaevasmina@gmail.com>
2024-07-16 17:35:40 +0500

    add program code

---
+++ code/Program.cs
@@ -0,0 +1,36 @@
+﻿void Main()
+{
+    string[] str = { "Hello", "2", "world", ":-)"};
+    PrintNewArray(str);
+}
+
+string[] FormNewArray(string[] str)
+{
+    int count = 0;
+    foreach (string s in str)
+    {
+        if (s.Length <= 3)
+            count++;
+    }
+
+    int index = 0;
+    string[] result = new string[count];
+    foreach (string s in str)
+    {
+       if (s.Length <= 3)
+        {
+            result[index] = s;
+            index++;
+        }
+    }
+    return result;
+}
+
+void PrintNewArray(string[] str)
+    {
+        string[] newArray = FormNewArray(str);
+        Console.WriteLine(string.Join(", ", newArray));
+    }
+
+
+Main();
