31ae1a6324caa485722d96efa71a781b4841afe1
Samina <dauylbaevasmina@gmail.com>
2024-09-14 22:21:10 +0500

    changed code

--- code/Program.cs
+++ code/Program.cs
@@ -1,35 +1,37 @@
 ﻿void Main()
 {
-    string[] str = { "Hello", "2", "world", ":-)"};
+    string[] str = { "Hello", "2", "world", ":-)" };
     PrintNewArray(str);
 }
 
 string[] FormNewArray(string[] str)
 {
     int count = 0;
-    for(int i=0; i <str.Length; i++ )
+    for (int i = 0; i < str.Length; i++)
     {
         if (str[i].Length <= 3)
             count++;
     }
 
     string[] result = new string[count];
-    for(int i=0; i <str.Length; i++ )
+    int index = 0; 
+    for (int i = 0; i < str.Length; i++)
     {
-       if (str[i].Length <= 3)
+        if (str[i].Length <= 3)
         {
-            result[i] = str[i];
-            i++;
+            result[index] = str[i];
+            index++;
         }
     }
     return result;
 }
 
 void PrintNewArray(string[] str)
-    {
-        string[] newArray = FormNewArray(str);
-        Console.WriteLine(string.Join(", ", newArray));
-    }
-
+{
+    string[] newArray = FormNewArray(str);
+    Console.WriteLine(string.Join(", ", newArray));
+}
 
 Main();
+
+
